70b64b10b5c0952d02a148706b63b203d5746a2b
Mystery-Man-From-Ouperspace <109296003+Mystery-Man-From-Ouperspace@users.noreply.github.com>
2023-12-28 13:59:06 +0100

    Initial commit

---
+++ modules/main.js
@@ -0,0 +1,149 @@
+// Import Modules
+import { conspiracyxActorSheet } from "./actor-sheet.js";
+import { conspiracyxActor } from "./actor.js";
+import { conspiracyxItem } from "./item.js";
+import { conspiracyxItemSheet } from "./item-sheet.js";
+import { conspiracyxCreatureSheet } from "./creature-sheet.js"
+import { conspiracyxVehicleSheet } from "./vehicle-sheet.js"
+
+/* -------------------------------------------- */
+/*  Foundry VTT Initialization                  */
+/* -------------------------------------------- */
+
+Hooks.once("init", async function() {
+    console.log(`Initializing CONX System`);
+
+    /**
+	 * Set an initiative formula for the system
+	 * @type {String}
+	 */
+	CONFIG.Combat.initiative = {
+        formula: "1d10 + @initiative.value",
+        decimals: 0
+      };
+
+      // Define Custom Entity Classes
+      CONFIG.Actor.documentClass = conspiracyxActor
+      CONFIG.Item.documentClass = conspiracyxItem
+
+      // Register sheet application classes
+      Actors.unregisterSheet("core", ActorSheet)
+
+      Actors.registerSheet("conspiracyx", conspiracyxActorSheet, 
+      {
+          types: ["character"],
+          makeDefault: true,
+          label: "Default CONX Character Sheet"
+      })
+
+      Actors.registerSheet("conspiracyx", conspiracyxCreatureSheet, 
+      {
+          types: ["creature"],
+          makeDefault: true,
+          label: "Default CONX Creature Sheet"
+      })
+
+      Actors.registerSheet("conspiracyx", conspiracyxVehicleSheet, 
+      {
+          types: ["vehicle"],
+          makeDefault: true,
+          label: "Default CONX Vehicle Sheet"
+      })
+
+      Items.registerSheet("conspiracyx", conspiracyxItemSheet, 
+      {
+          makeDefault: true,
+          label: "Default CONX Item Sheet"
+      })
+
+
+      // Game Settings
+      function delayedReload() {window.setTimeout(() => location.reload(), 500)}
+
+      game.settings.register("conspiracyx", "light-mode", {
+        name: "Light Mode",
+        hint: "Checking this option enables Light Mode, stripping away the dark mode aesthetics from the sheets.",
+        scope: "world",
+        config: true,
+        default: false,
+        type: Boolean,
+        onChange: delayedReload
+      });
+})
+
+/* -------------------------------------------- */
+/*  Chat Message Hooks                          */
+/* -------------------------------------------- */
+
+// Hook for Re-Rolls on Lucky/Unlucky Rolls
+Hooks.on("renderChatMessage", (app, html, data) => {
+    let chatButton = html[0].querySelector("[data-roll='roll-again']")
+
+    if (chatButton != undefined && chatButton != null) {
+        chatButton.addEventListener('click', () => {
+            let ruleTag = ''
+
+            if (html[0].querySelector("[data-roll='dice-result']").textContent == 10) {ruleTag = game.i18n.localize("CONX.Rule of Ten Re-Roll")}
+            if (html[0].querySelector("[data-roll='dice-result']").textContent == 1)  {ruleTag = game.i18n.localize("CONX.Rule of One Re-Roll")}
+
+            let roll = new Roll('1d10')
+            roll.roll({async: false})
+
+            // Grab and Set Values from Previous Roll
+            let attributeLabel = html[0].querySelector('h2').outerHTML
+            let diceTotal = Number(html[0].querySelector("[data-roll='dice-total']").textContent)
+            let rollMod = Number(html[0].querySelector("[data-roll='modifier']").textContent)
+            let ruleOfMod = ruleTag === 'Rule of Ten Re-Roll' ? Number(roll.result) > 5 ? Number(roll.result) - 5 : 0 : Number(roll.result) > 5 ? 0 : Number(roll.result) - 5
+            let ruleOfDiv = ''
+
+            if (roll.result == 10) {
+                ruleOfDiv = `<h2 class="rule-of-chat-text">Rule of 10!</h2>
+                            <button type="button" data-roll="roll-again" class="rule-of-ten">Roll Again</button>`
+                ruleOfMod = 5
+            }
+            if (roll.result == 1) {
+                ruleOfDiv = `<h2 class="rule-of-chat-text">Rule of 1!</h2>
+                            <button type="button" data-roll="roll-again" class="rule-of-one">Roll Again</button>`
+                ruleOfMod = -5
+            }
+
+            // Create Chat Content
+            let tags = [`<div>${ruleTag}</div>`]
+            let chatContent = `<form>
+                                    ${attributeLabel}
+
+                                    <table class="conspiracyx-chat-roll-table">
+                                        <thead>
+                                            <tr>
+                                                <th>`+game.i18n.localize(`CONX.Roll`)+`</th>
+                                                <th>`+game.i18n.localize(`CONX.Modifier`)+`</th>
+                                                <th>+</th>
+                                                <th>`+game.i18n.localize(`CONX.Result`)+`</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td data-roll="dice-result">[[${roll.result}]]</td>
+                                                <td data-roll="modifier">${rollMod}</td>
+                                                <td>+</td>
+                                                <td data-roll="dice-total">${diceTotal + ruleOfMod}</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+
+                                    <div style="display: flex; flex-direction: column; justify-content: center; align-items: center; width: 100%;">
+                                        ${ruleOfDiv}
+                                    </div>
+                                </form>`
+
+            ChatMessage.create({
+                type: CONST.CHAT_MESSAGE_TYPES.ROLL,
+                user: game.user.id,
+                speaker: ChatMessage.getSpeaker(),
+                flavor: `<div class="conspiracyx-tags-flex-container">${tags.join('')}</div>`,
+                content: chatContent,
+                roll: roll
+            })
+        })
+    }
+})
